4cc8aa17d101da1c0bb0c9fc311b9642911ade01
hamvai_ferenc <hamvai.ferenc@gmail.com>
2025-06-04 10:14:16 +0200

    Axios_added

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,11 +1,27 @@
 import React from "react";
+import axios from 'axios';
 
-function HomePage () {
- return (
-    <div>
-        <h2>Hello!</h2>
-    </div>
- )
+export default class ArtDatas extends React.Component{
+    state = {
+        arts: []
+    }
+
+componentDidMount(){
+    axios.get(`https://gist.githubusercontent.com/calvez/56bec47cb9c97d9999574adc65ef5368/raw/51f01cb2a160748d87dd40eae7d3785272a87355/all.json`)
+    .then(res => {
+        const arts = res.data;
+        this.setState({arts});
+    })
 }
 
-export default HomePage;
+render() {
+    return (
+        <ul>
+            {
+             this.state.arts.map(art => <li key={art.id}>{art.name}</li>
+             )
+            }
+        </ul>
+    )
+}
+}
